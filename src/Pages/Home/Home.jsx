e1220bc89dd0e2f3fe8327e3dbbeb5929edd9816
Joy <github.joysaha@gmail.com>
2024-05-01 12:05:32 +0600

    folder created

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
+
 
 const Home = () => {
-    return (
-        <div>
-            <h2>This is home</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h2>This is home</h2>
+      
+
+        
+       
+     
+    </div>
+  );
 };
 
 export default Home;
